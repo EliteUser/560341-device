942226219c3240d9a7208e10398cc6f24e2ed069
Andrey <unknownuser017@gmail.com>
2018-03-22 22:20:42 +0300

    Завершение проекта

--- js/script.js
+++ js/script.js
@@ -32,6 +32,9 @@ function windowPopup(open, window, close) {
   open.addEventListener("click", function (event) {
     event.preventDefault(event);
     window.classList.add("show");
+    window.classList.remove("animate-appear");
+    void window.offsetWidth;
+    window.classList.add("animate-appear");
   });
 
   close.addEventListener("click", function (event) {
@@ -48,125 +51,153 @@ for (var i = 0; i < popup.length; i++) {
   windowPopup(openBtn[i], popup[i], closeBtn[i]);
 };
 
-/*var sortUp = document.querySelector(".sort-up");
-var sortDown = document.querySelector(".sort-down");
+/* Упрощенная валидация формы */
 
-sortUp.addEventListener("click", function (event) {
-  event.preventDefault(event);
-  sortDown.classList.remove("sort-direction-active");
-  sortUp.classList.add("sort-direction-active");
-});
+var popupForm = document.querySelector(".popup-form");
+var popupSendBtn = document.querySelector(".popup-btn");
 
-sortDown.addEventListener("click", function (event) {
-  event.preventDefault(event);
-  sortUp.classList.remove("sort-direction-active");
-  sortDown.classList.add("sort-direction-active");
-});*/
+if (popupForm) {
+  popupSendBtn.addEventListener("click", function checkValidity() {
+    var inputs = [popupForm.elements.name, popupForm.elements.email, popupForm.elements.message];
 
-/* СЛАЙДЕР */
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].validity.valid == false) {
+        inputs[i].classList.add("input-invalid");
+        popup[1].classList.remove("animate-invalid");
+        void popup[1].offsetWidth;
+        popup[1].classList.add("animate-invalid");
+      } else {
+        inputs[i].classList.remove("input-invalid");
+      }
+    }
+  });
+}
 
-var sliderElem = document.getElementById("slider");
-var dragElemLeft = sliderElem.children[0];
-var dragElemRight = sliderElem.children[1];
-var selectedArea = sliderElem.children[2];
+/* Fallback для карты */
 
-function getCoords(elem) {
-  var box = elem.getBoundingClientRect();
-  return {
-    top: box.top + pageYOffset,
-    left: box.left + pageXOffset,
-    right: box.right + pageXOffset
+var mapSmall = document.getElementById("smallmap");
+if (mapSmall) {
+  mapSmall.onload = function () {
+    mapSmall.style.zIndex = 2;
   };
 }
 
-dragElemRight.onmousedown = function (e) {
-  var dragCoords = getCoords(dragElemRight);
-  var shiftX = e.pageX - dragCoords.left;
-  var sliderCoords = getCoords(sliderElem);
+var map = document.getElementById("bigmap");
+if (map) {
+  map.onload = function () {
+    map.style.zIndex = 2;
+  };
+}
 
-  document.onmousemove = function (e) {
-    var newLeft = e.pageX - shiftX - sliderCoords.left;
+/* СЛАЙДЕР В КАТАЛОГЕ */
 
-    var dragLeftRightCoord = getCoords(dragElemLeft).right - sliderCoords.left;
-    if (newLeft < dragLeftRightCoord) {
-      newLeft = dragLeftRightCoord;
-    }
+var sliderElem = document.getElementById("slider");
 
-    var rightEdge = sliderElem.offsetWidth - dragElemRight.offsetWidth / 2;
-    if (newLeft > rightEdge) {
-      newLeft = rightEdge;
-    }
+if (sliderElem) {
+  var dragElemLeft = sliderElem.children[0];
+  var dragElemRight = sliderElem.children[1];
+  var selectedArea = sliderElem.children[2];
 
-    selectedArea.style.left = dragLeftRightCoord - (dragElemLeft.offsetWidth / 2) + "px";
-    selectedArea.style.width = newLeft - dragLeftRightCoord + dragElemLeft.offsetWidth + "px";
+  function getCoords(elem) {
+    var box = elem.getBoundingClientRect();
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset,
+      right: box.right + pageXOffset
+    };
+  }
 
-    dragElemRight.style.left = newLeft + "px";
+  dragElemRight.onmousedown = function (e) {
+    var dragCoords = getCoords(dragElemRight);
+    var shiftX = e.pageX - dragCoords.left;
+    var sliderCoords = getCoords(sliderElem);
+
+    document.onmousemove = function (e) {
+      var newLeft = e.pageX - shiftX - sliderCoords.left;
+
+      var dragLeftRightCoord = getCoords(dragElemLeft).right - sliderCoords.left;
+      if (newLeft < dragLeftRightCoord) {
+        newLeft = dragLeftRightCoord;
+      }
+
+      var rightEdge = sliderElem.offsetWidth - dragElemRight.offsetWidth / 2;
+      if (newLeft > rightEdge) {
+        newLeft = rightEdge;
+      }
+
+      selectedArea.style.left = dragLeftRightCoord - (dragElemLeft.offsetWidth / 2) + "px";
+      selectedArea.style.width = newLeft - dragLeftRightCoord + dragElemLeft.offsetWidth + "px";
+
+      dragElemRight.style.left = newLeft + "px";
+    };
+
+    document.onmouseup = function () {
+      document.onmousemove = document.onmouseup = null;
+    };
+
+    return false;
   };
 
-  document.onmouseup = function () {
-    document.onmousemove = document.onmouseup = null;
+  dragElemRight.ondragstart = function () {
+    return false;
   };
 
-  return false; // disable selection start (cursor change)
-};
+  //////////////////////////////////////////////////////////////////////
 
-dragElemRight.ondragstart = function () {
-  return false;
-};
+  dragElemLeft.onmousedown = function (e) {
+    var dragCoords = getCoords(dragElemLeft);
+    var shiftX = e.pageX - dragCoords.left;
 
-//////////////////////////////////////////////////////////////////////
+    var sliderCoords = getCoords(sliderElem);
 
-dragElemLeft.onmousedown = function (e) {
-  var dragCoords = getCoords(dragElemLeft);
-  var shiftX = e.pageX - dragCoords.left;
+    document.onmousemove = function (e) {
+      var newLeft = e.pageX - shiftX - sliderCoords.left;
 
-  var sliderCoords = getCoords(sliderElem);
+      if (newLeft < -(dragElemLeft.offsetWidth / 2)) {
+        newLeft = -(dragElemLeft.offsetWidth / 2);
+      }
 
-  document.onmousemove = function (e) {
-    var newLeft = e.pageX - shiftX - sliderCoords.left;
+      var dragRightLeftCoord = getCoords(dragElemRight).left - sliderCoords.left - dragElemRight.offsetWidth;
+      if (newLeft > dragRightLeftCoord) {
+        newLeft = dragRightLeftCoord;
+      }
 
-    if (newLeft < -(dragElemLeft.offsetWidth / 2)) {
-      newLeft = -(dragElemLeft.offsetWidth / 2);
-    }
+      selectedArea.style.left = newLeft + (dragElemLeft.offsetWidth / 2) + "px";
+      selectedArea.style.width = dragRightLeftCoord - newLeft + dragElemLeft.offsetWidth + "px";
 
-    var dragRightLeftCoord = getCoords(dragElemRight).left - sliderCoords.left - dragElemRight.offsetWidth;
-    if (newLeft > dragRightLeftCoord) {
-      newLeft = dragRightLeftCoord;
-    }
+      dragElemLeft.style.left = newLeft + "px";
+    };
 
-    selectedArea.style.left = newLeft + (dragElemLeft.offsetWidth / 2) + "px";
-    selectedArea.style.width = dragRightLeftCoord - newLeft + dragElemLeft.offsetWidth + "px";
+    document.onmouseup = function () {
+      document.onmousemove = document.onmouseup = null;
+    };
 
-    dragElemLeft.style.left = newLeft + "px";
+    return false;
   };
 
-  document.onmouseup = function () {
-    document.onmousemove = document.onmouseup = null;
+  dragElemLeft.ondragstart = function () {
+    return false;
   };
-
-  return false; // disable selection start (cursor change)
-};
-
-dragElemLeft.ondragstart = function () {
-  return false;
-};
+}
 
 /* Фокус на карточке товара в каталоге */
 
 var catalogList = document.querySelector(".main-catalog");
 
-catalogList.addEventListener("focus", function (event) {
-  var target = event.target;
-  
-  if (target.classList.contains("add-to-cart-btn") || target.classList.contains("add-to-compare")) {
-    target.parentNode.classList.add("hover-show");
-  }
-}, true);
+if (catalogList) {
+  catalogList.addEventListener("focus", function (event) {
+    var target = event.target;
 
-catalogList.addEventListener("blur", function (event) {
-  var target = event.target;
-  
-  if (target.classList.contains("add-to-cart-btn") || target.classList.contains("add-to-compare")) {
-    target.parentNode.classList.remove("hover-show");
-  }
-}, true);
+    if (target.classList.contains("add-to-cart-btn") || target.classList.contains("add-to-compare")) {
+      target.parentNode.classList.add("hover-show");
+    }
+  }, true);
+
+  catalogList.addEventListener("blur", function (event) {
+    var target = event.target;
+
+    if (target.classList.contains("add-to-cart-btn") || target.classList.contains("add-to-compare")) {
+      target.parentNode.classList.remove("hover-show");
+    }
+  }, true);
+}
